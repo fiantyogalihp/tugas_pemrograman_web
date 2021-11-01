6811b7e2fa8b8c794b46843aa013661a09af804b
fiantyogalihp <fiantiogalih@gmail.com>
2021-11-01 23:20:20 +0700

    koneksi ke DB

--- kuliah/pertemuan10/latihan2.php
+++ kuliah/pertemuan10/latihan2.php
@@ -8,7 +8,6 @@ $pegawai = query("SELECT * FROM pegawai");
 <html lang="en">
 
 <head>
-
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
